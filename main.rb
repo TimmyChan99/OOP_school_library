08bf19c5691ea0609bbe68bc494beb176fe11e98
Fatima Ezzahra <felemenoun@gmail.com>
2022-04-08 14:03:56 +0100

    Create main method

--- main.rb
+++ main.rb
@@ -6,22 +6,44 @@ def start_app
   loop do
     menu_option = gets.chomp
     break if menu_option == '7'
+
+    options_list(menu_option)
   end
 end
 
 def menu
   [
-    'list all books',
-    'list all people',
-    'create a person',
-    'create a book',
-    'create a rental',
-    'list all rentals for a give person id',
-    'exit'
+    'List all books',
+    'List all people',
+    'Create a person',
+    'Create a book',
+    'Create a rental',
+    'List all rentals for a give person id',
+    'Exit'
   ].each_with_index { |option, index| puts "#{index + 1}- #{option}" }
 end
 
+def options_list(menu_option)
+  case menu_option
+  when '1'
+    list_all_books
+  when '2'
+    list_all_people
+  when '3'
+    create_person
+  when '4'
+    create_book
+  when '5'
+    create_rental
+  when '6'
+    list_rentals_for_id
+  else
+    puts 'Unvalid option'
+  end
+end
 
 def main
   app = App.new
+  app.run
+  start_app
 end
--- person.rb
+++ person.rb
@@ -38,10 +38,3 @@ class Person < Nameable
     Rental.new(book, date, self)
   end
 end
-
-person = Person.new(22, name: 'maximilianus')
-p person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-p capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-p capitalized_trimmed_person.correct_name
