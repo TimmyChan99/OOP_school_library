043fb2f725ff1123625e224c00396eac68f7933c
Fatima Ezzahra <felemenoun@gmail.com>
2022-04-13 13:58:13 +0100

    Test the Teacher class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,15 +1,14 @@
 require_relative 'spec_helper'
 
 describe Book do
-	before :each do
-		@book = Book.new 'Title', 'Author'
-	end
-
-	it 'It should return book title' do
-	  expect(@book.title) .to eq 'Title'
+  before :each do
+    @book = Book.new 'Title', 'Author'
   end
-	it 'It should return book author' do
-		expect(@book.author) .to eq 'Author'
-	end
 
+  it 'It should return book title' do
+    expect(@book.title).to eq 'Title'
+  end
+  it 'It should return book author' do
+    expect(@book.author).to eq 'Author'
+  end
 end
